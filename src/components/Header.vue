c83c3e1b160f9118322a1c52395b3a95a8fe885a
tankaio <tankaio@qq.com>
2019-09-03 20:57:36 +0800

    部分页头

---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <main class="container">
+      <div class="top">
+        <div class="top-left">
+          <span>欢迎光临依谷网</span>
+          <span>配送至：</span>
+          <select name="cities" id="">
+            <option value="">请选择城市</option>
+          </select>
+        </div>
+        <div class="top-right">
+          <ul>
+            <li>
+              <a href="javascript:;">免费注册</a>  
+            </li>  
+            <li>
+              <a href="javascript:;">登录</a>  
+            </li>  
+            <li>
+              <a href="javascript:;">购物车</a>  
+            </li>  
+            <li>
+              <a href="javascript:;">关注依谷网</a>  
+            </li>  
+            <li>
+              <a href="javascript:;">食品经营许可</a>  
+            </li>  
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1>test</h1>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+main{
+  font-size: 12px;
+}
+ul{
+  list-style-type: none;
+}
+a{
+  color: #4b4b4b;
+  text-decoration: none;
+}
+
+.top{
+  width: 1200px;
+  margin: 0 auto;
+  padding: 5px 0;
+}
+.top:after{
+  content: "";
+  display: block;
+  clear: both;
+}
+.top-left{
+  float: left;
+}
+.top-right{
+  float: right;
+}
+.top-right>ul{
+  margin: 0;
+  padding: 0;
+}
+/* .top-right>ul::after{
+  content: "";
+  display: block;
+  clear: both;
+} */
+.top-right>ul li{
+  float: left;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Header from './components/Header'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:"/Header",component:Header},
     {
       path: '/',
       name: 'home',
